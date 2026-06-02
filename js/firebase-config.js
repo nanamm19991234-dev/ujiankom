2714b4b5ba217e36876a840e3a89a138416b282e
nabilchyy-lab <muhammadnabilardyanda@gmail.com>
2026-06-02 10:18:51 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,24 +1,21 @@
-// Firebase Configuration
-// Project: monitoring-92e1e
-// PENTING: Jangan commit file ini ke repository publik.
-// Gunakan Firebase Security Rules untuk membatasi akses data.
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzBDdHWLMkWN7bH3oJBdri6s5KRBM6EHk",
-  authDomain: "monitoring-iot-29ac6.firebaseapp.com",
-  databaseURL: "https://monitoring-iot-29ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "monitoring-iot-29ac6",
-  storageBucket: "monitoring-iot-29ac6.firebasestorage.app",
-  messagingSenderId: "4724063661",
-  appId: "1:4724063661:web:0a03e34b61072f9fd97524",
-  measurementId: "G-2Y44SR9LMN"
+  apiKey: "AIzaSyAUtD1pjK69K5sNznjuxPzISWQzpbY182I",
+  authDomain: "ujikom-iot-2026-89010.firebaseapp.com",
+  databaseURL: "https://ujikom-iot-2026-89010-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ujikom-iot-2026-89010",
+  storageBucket: "ujikom-iot-2026-89010.firebasestorage.app",
+  messagingSenderId: "733070489597",
+  appId: "1:733070489597:web:8f8544337cdfcbf94c8b94",
+  measurementId: "G-2X1SP175YC"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const analytics = getAnalytics(app);
 export default app;
